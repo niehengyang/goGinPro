0ccd5ed7bea5259b0fed1467b087ed5671b94c5c
niehengyang <niehengyang@ebupt.com>
2020-10-21 15:58:15 +0800

    更新提交

--- Models/model3D.go
+++ Models/model3D.go
@@ -1,7 +1,6 @@
 package Models
 
 import (
-	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"goGinPro/Databases"
 )
@@ -51,13 +50,15 @@ func SaveModels(sceneId int, newModels []SceneToModel) bool {
 
 	Mysql.DB.Where("scene_id = ?", sceneId).Delete(&SceneToModel{})
 	Mysql.DB.Unscoped().Delete(&SceneToModel{})
+
+	var Null uint
 	for _, value := range newModels {
+		value.ID = Null
 		status := Mysql.DB.Create(&value)
 		// id := value.ID
 		if status.Error != nil {
 			return false
 		}
-		fmt.Print(value)
 	}
 	return true
 }
@@ -67,4 +68,9 @@ func GetSceneModels() []SceneToModel {
 	Mysql.DB.Find(&models)
 
 	return models
+}
+
+func DeleteModel(id string) bool {
+	Mysql.DB.Delete(&Models{}, id)
+	return true
 }
